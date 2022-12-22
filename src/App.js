3720ec680131f8104eaaa5d82cff1356b4fd30d6
Sébastien Mantanus <sebastienmantanus@iMac-de-Sebastien.local>
2022-12-22 22:30:50 +0100

    design commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import logo from "./assets/logo.png";
+import { useState } from "react";
 
 function App() {
+  const [switch1, setSwitch1] = useState(false);
+  const [switch2, setSwitch2] = useState(false);
+  const [switch3, setSwitch3] = useState(false);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <section className="header">
+          <img src={logo} alt="logo" />
+          <h1>Ready To Go</h1>
+        </section>
       </header>
+      <main>
+        <section className="container">
+          <section className="selectors">
+            <div>
+              <div className="selector">ON</div>
+              <div className="selector">ON</div>
+              <div className="selector selected">ON</div>
+            </div>
+            <div>
+              <div className="selector">OFF</div>
+              <div className="selector selected">OFF</div>
+              <div className="selector">OFF</div>
+            </div>
+          </section>
+          <section>
+            <div className="result red">NO WAY</div>
+          </section>
+        </section>
+      </main>
     </div>
   );
 }
